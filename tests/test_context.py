ac141d5a75d9aef2ef6d7594351a631b1b9a4ae3
Mike Boers <mboers@keypics.com>
2012-09-12 12:08:21 -0700

    Cleanup test_context

--- tests/test_context.py
+++ tests/test_context.py
@@ -14,59 +14,60 @@ class TestContext(TestCase):
         tasks = [shot.Task(step['code'] + ' something', step=step, entity=shot, project=proj) for step in steps for shot in shots]
         
         self.proj = minimal(proj)
-        self.seqs = [minimal(x) for x in seqs]
-        self.shots = [minimal(x) for x in shots]
-        self.steps = [minimal(x) for x in steps]
-        self.tasks = [minimal(x) for x in tasks]
+        self.seqs = map(minimal, seqs)
+        self.shots = map(minimal, shots)
+        self.steps = map(minimal, steps)
+        self.tasks = map(minimal, tasks)
 
         self.root = os.path.join('scratch', mini_uuid())
         self.session = Session(self.sg)
         self.sgfs = SGFS(root=self.root, session=self.session)
         
-    def test_context_from_entities(self):
+    def test_context_from_single_task(self):
         
-        shots = [self.session.merge(x) for x in self.tasks]
-        shots[0].pprint()
-        print
+        task = self.session.merge(self.tasks[0])
+        ctx = self.sgfs.context_from_entities([task])
         
-        ctx = self.sgfs.context_from_entities([shots[0]])
-        ctx.pprint()
-        print
-        
-        self.assert_(ctx.is_linear)
+        self.assertTrue(ctx.is_linear)
         self.assertEqual(len(ctx.linear_base), 4)
+        
         self.assertEqual(len(list(ctx.iter_leafs())), 1)
-        self.assertEqual(len(list(ctx.iter_by_type(('Project')))), 1)
-        self.assertEqual(len(list(ctx.iter_by_type(('Sequence')))), 1)
-        self.assertEqual(len(list(ctx.iter_by_type(('Shot')))), 1)
-        self.assertEqual(len(list(ctx.iter_by_type(('Task')))), 1)
+        self.assertSameEntity(next(ctx.iter_leafs()).entity, self.tasks[0])
         
-        ctx = self.sgfs.context_from_entities(shots)
-        ctx.pprint()
-        print
+        self.assertEqual(len(list(ctx.iter_by_type('Project'))), 1)
+        self.assertSameEntity(next(ctx.iter_by_type('Project')).entity, self.proj)
         
-        self.assert_(not ctx.is_linear)
+        self.assertEqual(len(list(ctx.iter_by_type('Sequence'))), 1)
+        self.assertSameEntity(next(ctx.iter_by_type('Sequence')).entity, self.seqs[0])
+        
+        self.assertEqual(len(list(ctx.iter_by_type('Shot'))), 1)
+        self.assertSameEntity(next(ctx.iter_by_type('Shot')).entity, self.shots[0])
+        
+        self.assertEqual(len(list(ctx.iter_by_type('Task'))), 1)
+        self.assertSameEntity(next(ctx.iter_by_type('Task')).entity, self.tasks[0])
+        
+        self.assertSameEntity(ctx.entity, self.proj)
+        self.assertSameEntity(ctx.children[0].entity, self.seqs[0])
+        self.assertSameEntity(ctx.children[0].children[0].entity, self.shots[0])
+        self.assertSameEntity(ctx.children[0].children[0].children[0].entity, self.tasks[0])
+    
+    def test_context_from_multiple_tasks(self):
+        
+        tasks = [self.session.merge(x) for x in self.tasks]
+        ctx = self.sgfs.context_from_entities(tasks)
+        
+        self.assertFalse(ctx.is_linear)
         self.assertEqual(len(ctx.linear_base), 1)
         self.assertEqual(len(list(ctx.iter_leafs())), 12)
-        self.assertEqual(len(list(ctx.iter_by_type(('Project')))), 1)
-        self.assertEqual(len(list(ctx.iter_by_type(('Sequence')))), 2)
-        self.assertEqual(len(list(ctx.iter_by_type(('Shot')))), 4)
-        self.assertEqual(len(list(ctx.iter_by_type(('Task')))), 12)
+        self.assertEqual(len(list(ctx.iter_by_type('Project'))), 1)
+        self.assertEqual(len(list(ctx.iter_by_type('Sequence'))), 2)
+        self.assertEqual(len(list(ctx.iter_by_type('Shot'))), 4)
+        self.assertEqual(len(list(ctx.iter_by_type('Task'))), 12)
             
     def test_linearize(self):
         
-        shots = [self.session.merge(x) for x in self.tasks]
-        ctx = self.sgfs.context_from_entities(shots)
-        ctx.pprint()
-        print
-        
-        self.assert_(not ctx.is_linear)
-        self.assertEqual(len(ctx.linear_base), 1)
-        self.assertEqual(len(list(ctx.iter_leafs())), 12)
-        self.assertEqual(len(list(ctx.iter_by_type(('Project')))), 1)
-        self.assertEqual(len(list(ctx.iter_by_type(('Sequence')))), 2)
-        self.assertEqual(len(list(ctx.iter_by_type(('Shot')))), 4)
-        self.assertEqual(len(list(ctx.iter_by_type(('Task')))), 12)
+        tasks = [self.session.merge(x) for x in self.tasks]
+        ctx = self.sgfs.context_from_entities(tasks)
         
         linearized = list(ctx.iter_linearized())
         for lin in linearized:
