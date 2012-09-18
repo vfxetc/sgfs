2cb0c469547d6f4d00926063cd86cd162dd91d85
Mike Boers <mboers@keypics.com>
2012-09-17 17:02:57 -0700

    First attempt at "Jump to Folder"; WIP

---
+++ sgfs/commands/open.py
@@ -0,0 +1,35 @@
+from subprocess import call
+import optparse
+import os
+import platform
+
+from shotgun_api3_registry import connect
+from sgfs import SGFS
+
+
+def sgaction(entity_type, selected_ids, **kwargs):
+    
+    root = os.environ.get('KS_PROJECTS')
+    if not root:
+        optparser.error('$KS_PROJECTS must be set')
+    sgfs = SGFS(root, shotgun=connect())
+    
+    paths = []
+    
+    for id_ in selected_ids:
+        entity = sgfs.session.merge(dict(type=entity_type, id=id_))
+        path = sgfs.path_for_entity(entity)
+        if path:
+            paths.append(path)
+    
+    if not paths:
+        print 'No paths for %s %s' % (entity_type, selected_ids)
+        return
+    
+    for path in set(paths):
+        if platform.system() == 'Darwin':
+            call(['open', path])
+        else:
+            call(['xdg-open', path])
+
+    
