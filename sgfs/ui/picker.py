4e454b393602e63d27e3926b43c137908515b8d4
Mike Boers <mboers@keypics.com>
2012-11-05 13:07:15 -0800

    Picker: WIP

--- sgfs/ui/picker.py
+++ sgfs/ui/picker.py
@@ -4,6 +4,8 @@
 import sys
 import os
 
+import concurrent.futures
+
 from PyQt4 import QtCore, QtGui
 Qt = QtCore.Qt
 
@@ -12,6 +14,7 @@ from sgfs import SGFS
 app = QtGui.QApplication(sys.argv)
 sgfs = SGFS()
 
+threadpool = concurrent.futures.ThreadPoolExecutor(4)
 
 # sys.path.append('/home/mboers/Documents/RemoteConsole')
 # import remoteconsole 
@@ -46,43 +49,124 @@ class TextOption(object):
             yield option, {self.key: option}
 
 
-class ShotgunQuery(object):
 
-    def __init__(self, *args):
-        self.args = args
-        
-    
-    def __call__(self, data):
-        for entity in sgfs.session.find(*self.args):
-            try:
-                label = entity_name[entity['type']].format(**entity)
-            except KeyError:
-                label = repr(entity)
-            yield label, {entity['type']: entity}
 
 
 class Node(object):
     
-    def __init__(self, parent, label, data, child_iter_func=None):
-        self.parent = parent
-        self.label = label
-        self.data = data
-        self.child_iter_func = child_iter_func
-        self.child_iter_called = False
+    @staticmethod
+    def is_next_node(state, goal_state):
+        return True
     
+    def __init__(self, model, parent, view_data, state, goal_state=None):
+        
+        if not self.is_next_node(state, goal_state):
+            raise KeyError('not next state')
+        
+        self.model = model
+        self.parent = parent
+        self.view_data = view_data or {}
+        self.state = state
+        self.goal_state = goal_state
+        
+        self.index = None # Set by the model.
+        
+        self.is_loading = False
+        self._children = None
+    
+    def has_children(self):
+        return True
+    
+    def fetch_children(self):
+        raise NotImplementedError()
+    
+    def update_children(self, new_children, signal=True):
+        
+        children = []
+        for view_data, new_state in new_children:
+            child_state = dict(self.state)
+            child_state.update(new_state)
+            child = self.model.get_next_node(self, view_data, child_state)
+            children.append(child)
+        
+        if signal:
+            self.model.beginInsertRows(self.index, 0, len(children))
+        
+        self._children = children
+        
+        if signal:
+            self.model.endInsertRows()
+    
+
     def children(self):
-        if not self.child_iter_called:
-            self.child_iter_called = True
-            self._children = []
-            
-            if self.child_iter_func:
-                for label, new_data in self.child_iter_func(self.data):
-                    child_data = dict(self.data)
-                    child_data.update(new_data)
-                    self._children.append(Node(self, label, child_data, model._get_iter(child_data)))
-            
-            self._children = tuple(self._children)
+        if self._children is None:
+            self.update_children(self.fetch_children(), signal=False)
         return self._children
+
+
+class LeafNode(Node):
+
+    def __init__(self, *args):
+        super(LeafNode, self).__init__(*args)
+        self.view_data['header'] = ''
+    
+    def has_children(self):
+        return False
+    
+    def fetch_children(self):
+        return []
+
+
+class ShotgunQuery(Node):
+    
+    entity_type = 'Project'
+    backref = (None, None)
+    
+    @classmethod
+    def build_basic(cls, entity_type, backref):
+        class Specialized(cls):
+            pass
+        Specialized.__name__ = '%s%s' % (entity_type, cls.__name__)
+        Specialized.entity_type = entity_type
+        Specialized.backref = backref
+        return Specialized
+    
+    @classmethod
+    def is_next_node(cls, state, goal_state):
+        if cls.entity_type not in state and (cls.backref is None or cls.backref[0] in state):
+            if goal_state is not None and cls.entity_type not in goal_state:
+                return False
+            return True
+        return False
+    
+    def __init__(self, *args):
+        super(ShotgunQuery, self).__init__(*args)
+        self.view_data['header'] = self.entity_type
+    
+    def fetch_children(self):
+        print 'fetching children'
+        future = threadpool.submit(self._fetch_children)
+        future.add_done_callback(lambda f: self.update_children(f.result()))
+        return []
+    
+    def _fetch_children(self):
+        filters = []
+        if self.backref is not None:
+            filters.append((self.backref[1], 'is', self.state[self.backref[0]]))
+        
+        for entity in sgfs.session.find(self.entity_type, filters):
+
+            print 'have', entity
+            try:
+                label = entity_name[entity['type']].format(**entity)
+            except KeyError:
+                label = repr(entity)
+                
+            view_data = {
+                Qt.DisplayRole: label,
+            }
+            
+            yield view_data, {entity['type']: entity}
 
 
 class Model(QtCore.QAbstractItemModel):
@@ -93,7 +177,21 @@ class Model(QtCore.QAbstractItemModel):
         super(Model, self).__init__()
         
         self._root = None
+        self.node_types = []
     
+    def get_next_node(self, parent, view_data, state):
+        for node_type in self.node_types:
+            try:
+                return node_type(self, parent, view_data, state)
+            except KeyError:
+                pass
+        return LeafNode(self, parent, view_data, state)
+        
+    def hasChildren(self, index):
+        node = self.node_from_index(index)
+        return node.has_children()
+        
+        
     def _get_iter(self, data):
         
         if 'Task' in data:
@@ -134,7 +232,8 @@ class Model(QtCore.QAbstractItemModel):
     
     def root(self):
         if self._root is None:
-            self._root = Node(None, '', {}, self._get_iter({}))
+            self._root = self.get_next_node(None, {}, {})
+            self._root.index = QtCore.QModelIndex()
         return self._root
     
     def node_from_index(self, index):
@@ -151,14 +250,10 @@ class Model(QtCore.QAbstractItemModel):
             return self._header
         
         return QtCore.QVariant()
-        
-    def hasChildren(self, parent):
-        return True
     
     def rowCount(self, parent):
-        
         node = self.node_from_index(parent)
-        return len(node.children())
+        return len(node.children()) if node is not None else 0
     
     def columnCount(self, parent):
         return 1
@@ -169,13 +264,15 @@ class Model(QtCore.QAbstractItemModel):
             return QtCore.QModelIndex()
         
         node = self.node_from_index(parent)
-        node.index = parent
         node_children = node.children()
         
         if row >= len(node_children):
             return QtCore.QModelIndex()
         
-        return self.createIndex(row, col, node_children[row])
+        child = node_children[row]
+        index = self.createIndex(row, col, node_children[row])
+        child.index = index
+        return index
     
     def parent(self, child):
 
@@ -185,6 +282,10 @@ class Model(QtCore.QAbstractItemModel):
         
         return node.parent.index
         
+    # def flags(self, index):
+    #     if not index.row():
+    #         return Qt.NoItemFlags
+    #     return super(Model, self).flags(index)
     
     def data(self, index, role):
         
@@ -193,7 +294,10 @@ class Model(QtCore.QAbstractItemModel):
         
         if role == Qt.DisplayRole:
             node = self.node_from_index(index)
-            return node.label
+            return node.view_data.get(Qt.DisplayRole, repr(node))
+        
+        # if role == Qt.DecorationRole:
+        #     return QtGui.QColor(256, 128, 64)
         
         else:
             # Invalid variant.
@@ -212,11 +316,39 @@ class Header(QtGui.QHeaderView):
         self.setResizeMode(0, QtGui.QHeaderView.Fixed)
         self.setStretchLastSection(True)
         
+        # self._timer = QtCore.QTimer()
+        # self._timer.singleShot(1000, self._upper_header)
+    
+    def _upper_header(self, *args):
+        print '_upper_header', args
+        self._name = self._name.upper()
+        self.model().headerDataChanged.emit(Qt.Horizontal, 0, 0)
+        
     def paintEvent(self, e):
         # Such a horrible hack.
         self.model()._header = self._name
         super(Header, self).paintEvent(e)
 
+
+class TestDelegate(QtGui.QStyledItemDelegate):
+    
+    def __init__(self):
+        super(TestDelegate, self).__init__()
+        self._pixmap = QtGui.QPixmap('/home/mboers/Documents/icons/fatcow/32x32/3d_glasses.png')
+    
+    def sizeHint(self, option, index):
+        hint = super(TestDelegate, self).sizeHint(option, index)
+        hint.setHeight(hint.height() + self._pixmap.height())
+        return hint
+    
+    def paint(self, painter, option, index):
+        option.displayAlignment = Qt.AlignTop
+        super(TestDelegate, self).paint(painter, option, index)
+        painter.drawPixmap(
+            (option.rect.width() - self._pixmap.width()) / 2,
+            option.rect.height() - self._pixmap.height(),
+            self._pixmap
+        )
 
 class TreeView(QtGui.QTreeView):
     
@@ -229,15 +361,13 @@ class TreeView(QtGui.QTreeView):
         self.setModel(model)
         self.setRootIndex(index)
         
-        
-        # Make this behave like a fancier ListView
+        # Make this behave like a fancier ListView. This also allows for the
+        # right arrow to select.
         self.setRootIsDecorated(False)
-        self.setExpandsOnDoubleClick(False)
-        
-        self.expanded.connect(lambda index: self.collapse(index))
+        self.setItemsExpandable(False)
 
-
-
+        # self._delegate = TestDelegate()
+        # self.setItemDelegateForRow(0, self._delegate)
 
 
 class ColumnView(QtGui.QColumnView):
@@ -250,7 +380,7 @@ class ColumnView(QtGui.QColumnView):
     def createColumn(self, index):
         
         node = self.model().node_from_index(index)
-        header = node.label or 'NO LABEL'
+        header = node.view_data.get('header', 'No Header')
 
         view = TreeView(self.model(), index, header)
         
@@ -273,7 +403,14 @@ class Dialog(QtGui.QDialog):
 
 
 obj = ColumnView()
+
 model = Model()
+model.node_types.append(ShotgunQuery.build_basic('Project', None))
+model.node_types.append(ShotgunQuery.build_basic('Sequence', ('Project', 'project')))
+model.node_types.append(ShotgunQuery.build_basic('Shot', ('Sequence', 'sg_sequence')))
+model.node_types.append(ShotgunQuery.build_basic('Task', ('Shot', 'entity')))
+
+
 obj.setModel(model)
 
 obj.show()
