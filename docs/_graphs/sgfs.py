484c189009610ebdcc5d67e39ba59b34fb057c8f
Mike Boers <mboers@keypics.com>
2012-11-20 12:12:19 -0800

    Docs tasks need steps

--- docs/_graphs/sgfs.py
+++ docs/_graphs/sgfs.py
@@ -8,8 +8,9 @@ def context_from_path():
     proj = fix.Project('Example Project')
     seq = proj.Sequence("AA")
     shot = seq.Shot('AA_001')
-    task = shot.Task('Do Work', id=123)
-    task2 = shot.Task('Do More Work', id=234)
+    step = fix.Step('Anm')
+    task = shot.Task('Do Work', id=123, step=step)
+    task2 = shot.Task('Do More Work', id=234, step=step)
     
     ctx = sgfs.context_from_entities([task])
     yield ctx.dot()
