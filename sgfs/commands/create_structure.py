d58256b7087b2995ecd6be82a4223629e221dd5a
Mike Boers <mikeb@markmedia.co>
2017-07-05 15:43:55 -0400

    Add support for Lofty Sky's custom entities.
    This may break other facilities, and needs to be improved with sgschema.

--- sgfs/commands/create_structure.py
+++ sgfs/commands/create_structure.py
@@ -16,7 +16,7 @@ class CreateStructureCommand(Command):
     def __init__(self):
         super(CreateStructureCommand, self).__init__()
         self.add_option('-n', '--dry-run', action='store_true', help='don\'t actually do anything')
-        self.add_option('-v', '--verbose', action='store_true', help='show everything being done')
+        self.add_option('-v', '--verbose', action='count', help='show everything being done', default=0)
         self.add_option('-t', '--types', action="append", dest="entity_types", help="entity types to find if given a path")
         
     def run(self, sgfs, opts, args):
@@ -30,6 +30,12 @@ class CreateStructureCommand(Command):
                 print >> sys.stderr, 'no entities found for', arg
             entities.append(entity)
         
+        if opts.verbose > 1:
+            context = sgfs.context_from_entities(entities)
+            context.pprint()
+            structure = sgfs.structure_from_entities(entities)
+            structure.pprint()
+
         sgfs.create_structure(entities, dry_run=opts.dry_run, verbose=opts.verbose or opts.dry_run)
 
 
