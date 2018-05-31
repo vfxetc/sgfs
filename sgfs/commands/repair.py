b1edcf5076d87210dbaf2fb28403115d87d8da59
Mike Boers <mikeb@loftysky.com>
2018-05-31 14:09:43 -0400

    sgfs-repair --dirmap old:new

--- sgfs/commands/repair.py
+++ sgfs/commands/repair.py
@@ -1,77 +1,97 @@
+import argparse
 import os
 
+from dirmap import DirMap
 from sgsession.utils import parse_isotime
 
-from . import Command
+from sgfs import SGFS
 
 
-class RepairCommand(Command):
-    """%prog [options] path+
+def main():
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-r', '--recurse', action='store_true')
+    parser.add_argument('-n', '--dry-run', action='store_true')
+    parser.add_argument('-v', '--verbose', action='count')
     
-    Find and relink entities into the SGFS cache.
-    
-    """
-    
+    parser.add_argument('-t', '--parse-times', action='store_true')
+    parser.add_argument('-d', '--dirmap', action='append', dest='_dirmap')
+
+    parser.add_argument('roots', nargs='+')
+
+    cmd = parser.parse_args(namespace=Repair())
+    cmd.run()
+
+
+class Repair(object):
+
     def __init__(self):
-        super(RepairCommand, self).__init__()
-        self.add_option('-r', '--recurse', action="store_true", dest="recurse")
-        self.add_option('-n', '--dry-run', action="store_true", dest="dry_run")
-        self.add_option('-v', '--verbose', action="store_true", dest="verbose")
-        
-    def run(self, sgfs, opts, args, recurse=False, **kwargs):
+        self._logs = {}
+        self.sgfs = SGFS()
 
-        if not args:
-            self.print_usage()
-            return 1
+    def log(self, message):
+        self._logs[message] = self._logs.get(message, 0) + 1
 
-        for root in args:
+    def run(self):
+
+        self.dirmap = DirMap(self._dirmap) if self._dirmap else None
+
+        for root in self.roots:
+
             root = os.path.abspath(root)
+            if self.recurse:
+                for path, _, _ in os.walk(root):
+                    self.repair_path(path)
+            else:
+                self.repair_path(root)
 
-            for path, dir_names, file_names in os.walk(root):
-                tags = sgfs._read_directory_tags(path)
-                if not tags:
-                    continue
-                if opts.verbose:
-                    print 'checking', path
+    def repair_path(self, path):
 
-                tags, repair_count = self.repair_tags(tags)
-                if repair_count:
-                    print 'REPAIRED', path
-                    if not opts.dry_run:
-                        sgfs._write_directory_tags(path, tags, replace=True)
+        tags = self.sgfs._read_directory_tags(path)
+        if not tags:
+            return
 
-    def repair_tags(self, data):
+        tags = self.repair(tags)
 
-        repair_count = 0
+        if self.verbose and (self.verbose > 1 or self._logs):
+            print path
+
+        if not self._logs:
+            return
+
+        for k, v in sorted(self._logs.items()):
+            print '    {}x {}'.format(v, k)
+
+        if not self.dry_run:
+            self.sgfs._write_directory_tags(path, tags, replace=True)
+
+    def repair(self, data):
 
         if isinstance(data, (list, tuple)):
-            new = []
-            for x in data:
-                x, new_count = self.repair_tags(x)
-                new.append(x)
-                repair_count += new_count
-            return new, repair_count
+            return [self.repair(x) for x in data]
 
         if isinstance(data, dict):
+
             if 'type' in data and 'id' in data:
-                for key in 'updated_at', 'created_at':
-                    value = data.get(key)
-                    if value and isinstance(value, basestring):
-                        data[key] = parse_isotime(value)
-                        repair_count += 1
-            new = {}
-            for key, value in data.iteritems():
-                new[key], new_count = self.repair_tags(value)
-                repair_count += new_count
-            return new, repair_count
+                self.repair_entity(data)
 
-        return data, 0
+            # Process the rest of it.
+            return {k: self.repair(v) for k, v in data.items()}
 
-        
+        if isinstance(data, basestring) and self.dirmap is not None:
+            new = self.dirmap(data)
+            if new != data:
+                self.log('{} -> {}'.format(data, new))
+                return new
 
+        return data
 
-main = RepairCommand()
+    def repair_entity(self, entity):
 
-def main_rebuild(*args, **kwargs):
-    kwargs['recurse'] = True
-    return main(*args, **kwargs)
+        if self.parse_times:
+            for key in 'updated_at', 'created_at':
+                value = entity.get(key)
+                if value and isinstance(value, basestring):
+                    entity[key] = parse_isotime(value)
+                    self.log('parsed {}[{}].{} == {}'.format(entity['type'], entity['id'], key, entity[key]))
+
